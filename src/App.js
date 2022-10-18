5afa8ce362194d4f8d56844d25200889757a7c8f
Robert Kelly <robert812@shaw.ca>
2022-10-18 12:43:00 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar";
 import Contact from "./Contact/Contact";
 import Skills from "./Skills/Skills";
@@ -11,47 +10,28 @@ import About from "./About/About";
 // Github
 // Contact Me
 
+
 export default function App() {	
-	const [about, setAbout] = useState(false);
-	const [skills, setSkills] = useState(false);
-	const [contact, setContact] = useState(false);
-	const [navbar, setNavbar] = useState(false);
-	
-	function renderAbout() {
-
-	}
-
-	function renderSkills() {
-	
-	}
-	
-	function renderContact() {
-	
-	}
-
-	
+	const [active, setActive] = useState("");	
+		
 
 	return (
 		<div className="app-container">
 	  		<div className="header-section">
-	  			<Header />
+				<div className="header-wrapper">
+					<h1>ROBERT KELLY</h1>
+				</div>
+				<div className="nav-section">
+					<button onClick={() => setActive("about")}>About Me</button>
+					<button onClick={() => setActive("skills")}>Skills</button>
+					<button onClick={() => setActive("contact")}>Contact</button>
+				</div>
 	  		</div>
-	  		<div className="navbar-section">
-				<button>navbar</button>
-	  			<Navbar />
-	  		</div>
-			<div className="about-section">
-				<button>about</button>
-				<About />	
-	  		</div>
-			<div className="skills-section">
-				<button>skills</button>
-				<Skills />
+			<div className="render-area">
+				{active === "about" && <About />}
+				{active === "skills" && <Skills />}
+				{active === "contact" && <Contact />}
 			</div>
-	  		<div className="contact-section">
-				<button>contact</button>
-	  			<Contact />
-	  		</div>
 		</div>
 	)
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,18 @@
+import React, { useState } from "react";
 import "./Header.css";
 
-export default function Header() {
+export default function Header({setAbout}) {
+	function setAbout() {
+		props.setAbout(;
+	}		
+
 	return (
 		<div className="header-wrapper">
 			<h1>ROBERT KELLY</h1>
 			<div className="nav-section">
-				<button onClick={}>About Me</button>
-				<button onClick={}>Skills</button>
-				<button onClick={}>Contact Me</button>
+				<button onClick={setAbout}>About Me</button>
+				<button onClick={() => alert("skills")}>Skills</button>
+				<button onClick={() => alert("contact")}>Contact Me</button>
 			</div>
 		</div>
 	);
