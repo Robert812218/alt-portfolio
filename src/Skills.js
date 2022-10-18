8e7f742414880f2cd7814ccaf7a45f01376a3408
Robert Kelly <robert812@shaw.ca>
2022-10-18 17:43:12 -0600

    update

--- src/Skills.js
+++ src/Skills.js
@@ -2,8 +2,37 @@ import "./Skills.js";
 
 
 export default function Skills() {
-	return (
+	function renderEducation() {
+		return (
+			<div>
+			<h1>EDUCATION</h1>
+				<h3>Full Stack Web Development (Diploma) - Lighthouse Labs</h3>
+				<ul>
+					<li>The Command Line</li>
+					<li>Version Control</li>
+					<li>Javascript Fundamentals</li>
+					<li>Debugging</li>
+					<li>Asynchronous Code</li>
+					<li>NPM & Package Management</li>
+					<li>Test-Driven Development</li>
+					<li>Git & Git Workflow</li>
+					<li>Responsive Design</li>
+					<li>Client-side JS</li>
+				</ul>
+				<h3>Notable Learning Outcomes: </h3>
+					<ul>
+						<li><b>Web Development: </b>Acquired in-depth knowledge necessary to build dynamic web applications using various programming languages, frameworks, and tools. Demonstrates ability to build and deploy functional, database-driven web applications that are both visually appealing and optimized for search engine visibility.</li>
+						<li><b>Testing & Debugging: </b>Managed aspects of testing and debugging in an agile development environment of the functionality of a website and web-based application, including user interface confirmation, error identification, and resolution of programming issues.</li>
+							<li><b>Database Management: </b>Cultivated foundation in database management, learned to design databases, write SQL queries, and develop strategies to optimize performance. Gained valuable experience with Ruby on Rails and Node.js frameworks while executing a number of full-stack web development, data science, and design projects for Lighthouse Labs students and faculty.</li>
+						<li><b>Client-side Programming: </b>Solidified expertise in create interactive web applications using the latest client-side programming technologies, including HTML5, CSS3, and JavaScript. Devised new methodologies to streamline software development lifecycle while also reducing costs and time of deployment to production servers.</li>
+					</ul>
+			</div>
+		);	
+	}
+
+	function renderSkills() {
 		<div>
+			
 			<h1>SKILLS</h1>
 			        <ul>
                                         <li>UX & UI Design</li>
@@ -16,6 +45,17 @@ export default function Skills() {
                                         <li>Solution Development</li>
                                         <li>Team Communication</li>
                                 </ul>
+		</div>
+	}
+
+	function renderTechnologies() {
+		<div>
+			<h1>TECHNOLOGIES</h1>
+		</div>
+	}
+
+	return (
+		<div>
 		</div>
 	);
 }
