48b9aa3e00dd2279ba1daed8f0fb7224ce905067
Robert Kelly <robert812@shaw.ca>
2022-10-22 12:31:58 -0600

    update

--- src/App.js
+++ src/App.js
@@ -29,44 +29,6 @@ function OutlineTypesExample() {
   );
 }
 
-function DropdownExample() {
-	  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
-function ContainerExample() {
-	return (
-		<div>
-			<h4>React-bootstrap container component</h4>
-			<Container>
-				<h3>Sample Container</h3>
-			</Container>
-		</div>
-	);
-}
 
 export default function App() {	
 	const [active, setActive] = useState("");
--- src/SkillsAndTechs.js
+++ src/SkillsAndTechs.js
@@ -3,75 +3,40 @@ import Education from "./Education";
 import Skills from "./Skills";
 import Technologies from "./Technologies";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
-
-function BasicExample() {
-  return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-  );
-}
 
 
 export default function SkillsAndTechnologies() {
-	const [view, setView] = useState("");
-  	
- 	 	
-  
-  	return (
-  		<div className="skills-and-techs-container">
-  				<div className="education-area">
-  					<h2>EDUCATION</h2>
-  					<button onClick={() => setView("education")}>Education</button>					 
-  						{view === "education" && 
- 							<div>
-  								<Education />
- 							</div>
-  						}
-  				</div>
-  				<div className="skills-area">
-  				<h2>SKILLS</h2>
-  				<button onClick={() => setView("skills")}>Skills</button>
-  					{view === "skills" &&
-  						<div>
-  							<Skills />
-  						</div>
-  					}
-  				</div>
-  				<div className="technologies-area">
-  				<h2>TECHNOLOGIES</h2>
-  				<button onClick={() => setView("technologies")}>Expand</button>
-  					{view === "technologies" && 
-  						<div>
-  							<Technologies />
-  						</div>
-  					}
-  				</div>
-  		</div>
-  	);
-}
+ 	const [view, setView] = useState("");
+ 	  
+ 	
+ 
+   	return (
+   		<div className="skills-and-techs-container">
+   				<button onClick={() => setView("education")}>Education</button>					 
+   				<button onClick={() => setView("skills")}>Skills</button>
+ 			
+   				<button onClick={() => setView("technologies")}>Expand</button>
+   				{view === "education" && 
+  					<div>
+   						<Education />
+  					</div>
+   				}
+   				{view === "skills" &&
+   					<div>
+   						<Skills />
+   					</div>
+   				}
+   				{view === "technologies" && 
+   					<div>
+   						<Technologies />
+   					</div>
+   				}
+   		</div>
+   	);
+ }
