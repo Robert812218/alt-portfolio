05ef44701614e7efbd57ffca22ff40912fc9ae59
Robert812218 <robert812@shaw.ca>
2022-10-22 14:23:06 -0600

    update projects to include accordion bootstrap

--- src/Projects.js
+++ src/Projects.js
@@ -1,80 +1,103 @@
 import { useState } from "react";
 import "./Projects.css";
+import Accordion from "react-bootstrap/Accordion";
+
 
 function Reverb() {
 	return (
-		<div>
-			<h2>Reverb (contributor):</h2>
-				<p>A music player that connects to the Spotify Developer API. Lets users connect their Spotify accounts and play from their library, as well as to add and remove songs, albums, and playlists from their Spotify accounts, and Spotify's library.</p>
-				<a href="https://github.com/nathanpare/reverb-project">Reverb</a>
-		</div>
+		<Accordion defaultActiveKey="0">
+			<Accordion.Item eventKey="0">
+				<Accordion.Header>Reverb</Accordion.Header>
+				<Accordion.Body>
+					<p>A music player that connects to the Spotify Developer API. Lets users connect their Spotify accounts and play from their library, as well as to add and remove songs, albums, and playlists from their Spotify accounts - and their Spotify library.</p>
+					<a href="https://github.com/nathanpare/reverb-project">Reverb</a>
+				</Accordion.Body>
+			</Accordion.Item>
+		</Accordion>
 	);
 }
 
 function Scheduler() {
 	return (
-		<div>
+		<Accordion defaultActiveKey="0">
+			<Accordion.Item eventKey="0">
+				<Accordion.Header>Scheduler</Accordion.Header>
 			
-			<h2>Scheduler</h2>	
 				<p>A SPA (Single Page Application) for teachers to track their students interviews. The application uses various React hooks; from built-in to custom made. Users are enabled to add, edit and delete appointments in real time. Data is persisted using a PostgreSQL database.</p>
 				<a href="https://github.com/Robert812218/scheduler">Scheduler</a>
-		</div>
+			</Accordion.Item>	
+		</Accordion>
 	);
 }
+
 
 function LightBNB() {
-	return (
-		<div>
-			<h2>LightBNB</h2>
-				<p>An AirBNB clone built with SASS, Express.js, and PostgreSQL. Users can view, rent, add and review properties.</p>
-				<a href="https://github.com/Robert812218/scheduler">LightBNB</a>
-		</div>
+        return (
+                <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                                <Accordion.Header>LightBNB</Accordion.Header>
+				<Accordion.Body>
+					<p>An AirBNB clone built with SASS, Express.js, and PostgreSQL. Users can view, rent, add and review properties.</p>
+					<a href="https://github.com/Robert812217/scheduler">LightBNB</a>
+				</Accordion.Body>	
+                        </Accordion.Item>
+                </Accordion>
 	);
 }
+
 
 function Tweeter() {
-	return (
-		<div>
-			<h2>Tweeter</h2>
-				<p>A Twitter clone using HTML, CSS, JS, JQuery and AJAX. Back-end written in Node.js and Express.js.</p>
-				<a href="https://github.com/Robert812218/tweeter">Tweeter</a>
-		</div>
+        return (
+                <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                                <Accordion.Header>Tweeter</Accordion.Header>
+				<Accordion.Body>	
+					<p>A Twitter clone using HTML, CSS, JS, JQuery and AJAX. Back-end written in Node.js and Express.js.</p>
+					<a href="https://github.com/Robert812218/tweeter">Tweeter</a>
+				</Accordion.Body>
+                        </Accordion.Item>
+                </Accordion>
 	);
 }
 
+
 function Tinyapp() {
-	return (
-		<div>
-			<h2>Tinyapp</h2>
+        return (
+                <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                                <Accordion.Header>Tinyapp</Accordion.Header>
+
 				<p>A full-stack web application that allows users to shorten long URLs. Built with Node.js and Express.js.</p>
 				<a href="https://github.com/Robert812218/tinyapp">Tinyapp</a>
-		</div>
+                        </Accordion.Item>
+                </Accordion>
 	);
 }
+
+
 
 export default function Projects() {
 	const [view, setView] = useState("");
-	
 
 	return (
 		<div className="projects-container">
-			<h1>Github.com/Robert812218</h1>
 			<h1>PROJECTS</h1>
-				<div className="project-wrapper">
-					<Reverb />
-				</div>
-				<div className="project-wrapper">
-					<Scheduler />
-				</div>
-				<div className="project-wrapper">
-					<LightBNB />
-				</div>
-				<div className="project-wrapper">
-					<Tweeter />
-				</div>
-				<div className="project-wrapper">
-					<Tinyapp />	
-				</div>
+			<div className="project-wrapper">
+				<Reverb />
+			</div>
+			<div className="project-wrapper">
+				<Scheduler />
+			</div>
+			<div className="project-wrapper">
+				<LightBNB />		
+			</div>
+			<div className="project-wrapper">
+				<Tinyapp />
+			</div>
+			<div className="project-wrapper">
+				<Tweeter />
+			</div>
 		</div>
 	);
+
 } 
