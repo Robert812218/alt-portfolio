2ad0eafdd000939e7206a2a494bc81da1d7f7bc6
Robert Kelly <robert812@shaw.ca>
2022-10-23 15:23:05 -0600

    update

--- src/SkillsAndTechs.js
+++ src/SkillsAndTechs.js
@@ -11,42 +11,23 @@ import './SkillsAndTechs.css';
 
 function Technologies() {
 	return (
-		<div className="technology-component">
-			
+		<div className="technologies-container">
+			<ul> 
+				<li>Javascript Fundamentals</li>		
+				<li>HTML/CSS</li>
+				<li>Node.js</li>
+				<li>Express.js</li>
+				<li>React.js</li>
+				<li>Rust</li>	
+				<li></li>
+				<li>Elixir</li>
+				<li>Linux Operating Systems</li>
+				<li>Bash shell scripting</li>
+			</ul>
 		</div>
 	);
 }
 
-function Education() {
-        return (
-
-          <div className="education-component">
-                                <h1>EDUCATION</h1>
-                                        <h3>Full Stack Web Development (Diploma) - Lighthouse Labs</h3>
-                                        <ul>
-                                                <li>The Command Line</li>
-                                                <li>Version Control</li>
-                                                <li>Javascript Fundamentals</li>
-                                                <li>Debugging</li>
-                                                <li>Asynchronous Code</li>
-                                                <li>NPM & Package Management</li>
-                                                <li>Test-Driven Development</li>
-                                                <li>Git & Git Workflow</li>
-                                                <li>Responsive Design</li>
-                                                <li>Client-side JS</li>
-                                        </ul>
-                                        <h3>Notable Learning Outcomes: </h3>
- <ul>
-                                                        <li><b>Web Development: </b>Acquired in-depth knowledge necessary to build dynamic web applications using various programming languages, frameworks, and tools. Demonstrates ability to build and deploy functional, database-driven web applications that are both visually appealing and optimized for search engine visibility.</li>
-                                                        <li><b>Testing & Debugging: </b>Managed aspects of testing and debugging in an agile development environment of the functionality of a website and web-based application, including user interface confirmation, error identification, and resolution of programming issues.</li>
-                                                                <li><b>Database Management: </b>Cultivated foundation in database management, learned to design databases, write SQL queries, and develop strategies to optimize performance. Gained valuable experience with Ruby on Rails and Node.js frameworks while executing a number of full-stack web development, data science, and design projects for Lighthouse Labs students and faculty.</li>
-                                                        <li><b>Client-side Programming: </b>Solidified expertise in create interactive web applications using the latest client-side programming technologies, including HTML5, CSS3, and JavaScript. Devised new methodologies to streamline software development lifecycle while also reducing costs and time of deployment to production servers.</li>
-                                                </ul>
-                                </div>
-
-
-        );
-}
 
 function Skills() {
         return (
