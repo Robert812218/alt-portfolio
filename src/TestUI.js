9e6278dd3f3d66c9e1ca039ff6b5c5ca081f2740
Robert Kelly <robert812@shaw.ca>
2022-10-30 14:47:36 -0600

    update

--- src/TestUI.js
+++ src/TestUI.js
@@ -40,7 +40,84 @@ const About = props => {
 						<p>Full stack developer etc etc</p>
 						<p>More bio details</p>
 					</div>
+					<div className="title">
+						<h3>WHAT DO I DO</h3>
+						<p className="seperator" />
+					</div>
+					<div className="desc">
+						<h4>PROGRAMMER MANS</h4>
+						<p>
+							Front-end technologies
+						</p>
+						<p>
+							Back-end technologies
+						</p>
+					</div>
+					<div className="desc">
+						<h4 className="subtitle">Other things</h4>
+						<p>
+							Other techs
+						</p>
+						<p>
+							Other skills
+						</p>
+						
+					</div>
 				</article>
+			</div>
+		</section>
+	);
+}
+
+const Project = props => {
+	const tech = {
+		sass: 'fab fa-sass',
+		css: 'fab fa-css3-alt',
+		js: 'fab fa-js-square',
+		react: 'fab fa-react',
+		vue: 'fab fa-vuejs',
+		d3: 'far fa-chart-bar',
+		node: 'fab fa-node',
+	}
+
+	const link = props.link || 'http://';
+	const repo = props.repo || 'http://';
+	
+	return (
+		<div className="project">
+			<a className="project-link" href={link} target="_blank" rel="noopener noreferrer">
+				<img className="project-image" src={props.img} alt={'Screenshot of ' + props.title} />
+			</a>
+			<div className="project-details">
+				<div className="project-tile">
+					<p className="icons">
+						{props.tech.split(' ').map(t => (
+							<i className={tech[t]} key={t} />
+						))}
+					</p>
+					{props.title}{' '}
+				</div>
+				{props.children}
+				<div className="buttons">
+					<a href={repo} target="_blank" rel="noopener noreferrer">
+						View source <i className="fas fa-external-link-alt" />
+					</a>
+					<a href={link} target="_blank">
+						Try it Live <i className="fas fa-external-link-alt" />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+const Projects = props => {
+	return (
+		<section id="projects">
+			<div className="projects-container">
+				<div className="heading">
+					
+				</div>
 			</div>
 		</section>
 	);
